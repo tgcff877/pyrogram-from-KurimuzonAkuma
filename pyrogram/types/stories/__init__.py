01739afa4fe15fb1509bf1b67585f11fd1f1ce9b
Derkown <coder@derkown.ru>
2023-07-30 21:58:17 +0300

    update __init__.py

--- pyrogram/types/stories/__init__.py
+++ pyrogram/types/stories/__init__.py
@@ -1,6 +1,14 @@
 from .all_stories import AllStories
+from .stories import Stories
+from .story_views import StoryViews
+from .story_views_list import StoryViewsList
+from .user_stories import UserStories
 
 
 __all__ = [
     "AllStories",
+    "Stories",
+    "StoryViews",
+    "StoryViewsList",
+    "UserStories"
 ]
